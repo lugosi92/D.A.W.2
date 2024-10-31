08e1c8b759cb614eb463241b19cd123cc8e7cea5
lugosi92 <khaledbeno922@gmail.com>
2024-10-31 14:07:51 +0100

    PHP 31/10

--- PHP/Tema4/repintado/index.php
+++ PHP/Tema4/repintado/index.php
@@ -1,16 +1,28 @@
 <?php
 session_start();
-$errNom = $errCont = $errTipo = $errCheck = $errAnio = $$errCiudades = "";
+
+$nombre = $contraseña =$tipo = $año =  $ciudad = "";
+$check = array(); 
+$check2 = array(); 
+$errNom = $errCont = $errTipo = $errCheck = $errAnio = $errCiudades = "";
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     
 
+    // Para guardar los valores e inicializar 
 
-    // Para guardar los valores 
+        // INICIO DE SESION
         $nombre = isset($_POST['nombre']) ? $_POST['nombre'] : "";
         $contraseña = isset($_POST['contraseña']) ? $_POST['contraseña'] : "";
+
+        //RADIO
         $tipo = isset($_POST['tipo']) ? $_POST['tipo'] : "";
+
+        //CHECKBOX
         $check = isset($_POST['check']) ? $_POST['check'] : array();
+        $check2 = isset($_POST['check2']) ? $_POST['check2'] : array();
+
+        //SELECT
         $año = isset($_POST['año']) ? $_POST['año'] : "";
         $ciudad =isset($_POST['ciudad']) ? $_POST['ciudad'] : "";
 
@@ -31,8 +43,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         }
 
         //CHECKBOX
-        if(count($_POST['check'])<=0){
+        if(count($check) <= 0){
             $errCheck = "Recibir la publicidad";
+        }
+        if(count($check2) <= 0){
+            $errCheck2 = "Recibir la publicidad";
         }
 
         //SELECT
@@ -103,9 +118,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <fieldset class = "radio">
         <legend>RADIO</legend>
 
-            <input type= "radio" name = "tipo" value = "rojo" <?php if ($tipo== 'rojo') echo 'checked'; ?>>  Rojo
-            <input type= "radio" name = "tipo" value = "naranja" <?php if ($tipo== 'naranja') echo 'checked'; ?>>  Naranja 
-            <input type= "radio" name = "tipo" value = "verde" <?php if ($tipo== 'verde') echo 'checked'; ?>> Verde 
+            <input type= "radio" name = "tipo" value = "rojo" <?php if ($tipo == 'rojo') echo 'checked'; ?>>  Rojo
+            <input type= "radio" name = "tipo" value = "naranja" <?php if ($tipo == 'naranja') echo 'checked'; ?>>  Naranja 
+            <input type= "radio" name = "tipo" value = "verde" <?php if ($tipo == 'verde') echo 'checked'; ?>> Verde 
             <!-- ERROR TIPO -->
             <span class = "error"><?php echo $errTipo?></span>
     </fieldset>
@@ -113,9 +128,13 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <!-- CHECKBOX -->
     <fieldset class = "checkbox">
         <legend>CHECKBOX</legend>
-
             Quiero recibir publicidad<input type ="checkbox" value = "publicidad" name = "check[]" 
-            <?php if (in_array("publicidad", $_POST['check'])) echo 'checked'; ?>>
+            <?php if (isset($check) && in_array("publicidad", $check)) echo 'checked'; ?>>
+
+            Quiero recibir publicidad de verdad<input type ="checkbox" value = "publicidad2" name = "check2[]" 
+            <?php if (isset($check2) && in_array("publicidad", $check2)) echo 'checked'; ?>>
+
+
             <!-- ERROR CHECK -->
             <span class = "error"><?php echo $errCheck?></span>
     </fieldset>
@@ -127,18 +146,18 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 <label for = "fecha">Año de finalizacion de estudios:
                 <select id = "año" name = "año">
                     <option></option>
-                    <option value = "2021">2021</option>
-                    <option value = "2020">2020</option>
-                    <option value = "2019">2019</option>
-                    <option value = "2018">2018</option>
-                    <option value = "2017">2017</option>
+                    <option value = "2021" <?php if ($año == '2021') echo 'selected'; ?>>2021</option>
+                    <option value = "2020" <?php if ($año == '2020') echo 'selected'; ?>>2020</option>
+                    <option value = "2019" <?php if ($año == '2019') echo 'selected'; ?>>2019</option>
+                    <option value = "2018" <?php if ($año == '2018') echo 'selected'; ?>>2018</option>
+                    <option value = "2017" <?php if ($año == '2017') echo 'selected'; ?>>2017</option>
                 </select>
                 <!-- ERROR AÑOS -->
                 <span class = "error"><?php echo $errAnio?></span>
 
 
             <h2>Multiple:</h2>
-                <select id = "ciudad" name = "ciudad[]" multiple size="3">
+                <select id = "ciudad" name = "ciudad">
                     <option value="Madrid">Madrid</option>
                     <option value="Barcelona">Barcelona</option>
                     <option value="Valencia">Valencia</option>
