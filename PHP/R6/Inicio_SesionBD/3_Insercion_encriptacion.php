01e31a9929fde224edd49bee52d437cbf086744b
lugosi92 <khaledbeno922@gmail.com>
2024-11-15 14:11:32 +0100

    PHP

--- PHP/R6/Inicio_SesionBD/3_Insercion_encriptacion.php
+++ PHP/R6/Inicio_SesionBD/3_Insercion_encriptacion.php
@@ -6,10 +6,12 @@ if ($mysqli->connect_errno) {
 }
 echo $mysqli->host_info . "\n";
 
+// Cifrar contraseñas
 $contraseña = password_hash("1234", PASSWORD_DEFAULT);
 $contraseña1 = password_hash("5678", PASSWORD_DEFAULT);
 $contraseña2 = password_hash("9876", PASSWORD_DEFAULT);
 $contraseña3 = password_hash("5432", PASSWORD_DEFAULT);
+
 // INSERT
 $query = "INSERT INTO `usuario` (`usuario`, `contraseña`) VALUES
         ('Neil Molina', '$contraseña'),
