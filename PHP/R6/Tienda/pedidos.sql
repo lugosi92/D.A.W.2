58b5b24a2f228fc6196f916329f779c50e280b5f
lugosi92 <khaledbeno922@gmail.com>
2024-11-19 14:07:17 +0100

    Tienda

---
+++ PHP/R6/Tienda/pedidos.sql
@@ -0,0 +1,103 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 19-11-2024 a las 13:59:03
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `pedidos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria`
+--
+
+CREATE TABLE `categorias` (
+  `CodCat` int(11) NOT NULL,
+  `Nombre` varchar(45) NOT NULL,
+  `Descripcion` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedidos`
+--
+
+CREATE TABLE `pedidos` (
+  `CodPed` int(11) NOT NULL,
+  `Fecha` date NOT NULL,
+  `Enviado` int(11) NOT NULL,
+  `Restaurante` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedidosproductos`
+--
+
+CREATE TABLE `pedidosproductos` (
+  `CodPedProd` int(11) NOT NULL,
+  `Pedido` int(11) NOT NULL,
+  `Producto` int(11) NOT NULL,
+  `Unidades` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `CodProd` int(11) NOT NULL,
+  `Nombre` varchar(45) NOT NULL,
+  `Descripcion` varchar(90) NOT NULL,
+  `Peso` double NOT NULL,
+  `Stock` int(11) NOT NULL,
+  `Categoria` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `restaurantes`
+--
+
+CREATE TABLE `restaurantes` (
+  `CodRes` int(11) NOT NULL,
+  `Correo` varchar(90) NOT NULL,
+  `Clave` varchar(45) NOT NULL,
+  `Pais` varchar(45) NOT NULL,
+  `CP` int(11) NOT NULL,
+  `Ciudad` varchar(45) NOT NULL,
+  `Direccion` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `restaurantes`
+--
+
+INSERT INTO `restaurantes` (`CodRes`, `Correo`, `Clave`, `Pais`, `CP`, `Ciudad`, `Direccion`) VALUES
+(1, 'khaled@gmail.com', '1234', 'España', 28942, 'Madrid', 'Gran Via');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
