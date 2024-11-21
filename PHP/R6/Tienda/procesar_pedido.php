607782cf76cdec0658ccd96b140824a72028ccd7
lugosi92 <khaledbeno922@gmail.com>
2024-11-21 03:41:19 +0100

    PHP

--- PHP/R6/Tienda/procesar_pedido.php
+++ PHP/R6/Tienda/procesar_pedido.php
@@ -3,6 +3,42 @@
 	require 'sesiones.php';
 	require_once 'bd.php';
 	comprobar_sesion();
+
+	function insertar_pedido($carrito, $codRes) {
+		$res = leer_config(dirname(__FILE__)."/configuracion.xml", dirname(__FILE__)."/configuracion.xsd");
+		$bd = new PDO($res[0], $res[1], $res[2]);
+		$bd->beginTransaction();
+		$hora = date("Y-m-d H:i:s", time());
+		// insertar el pedido
+		$sql = "insert into pedidos(fecha, enviado, restaurante)
+				values('$hora', 0, $codRes)";
+		$result = $bd->query($sql);
+		if (!$result) {
+			return FALSE;
+		}
+		// coger el id del nuevo pedido para las filas detalle
+		$pedido = $bd->lastInsertId();
+		// insertar las filas en pedidoproductos
+		foreach($carrito as $codProd => $unidades) {
+			$sql = "insert into pedidosproductos(Pedido, Producto, Unidades)
+					values($pedido, $codProd, $unidades)";
+			//echo $sql;
+			$result = $bd->query($sql);
+			if (!$result) {
+				$bd->rollback();
+				return FALSE;
+			}
+			$sql = "update productos set stock = stock - $unidades
+					where codProd = $codProd";
+			$result = $bd->query($sql);
+			if (!$result) {
+				$bd->rollback();
+				return FALSE;
+			}
+		}
+		$bd->commit();
+		return $pedido;
+	}
 ?>	
 <!DOCTYPE html>
 <html>
--- PHP/R6/Tienda/productos.php
+++ PHP/R6/Tienda/productos.php
@@ -4,6 +4,35 @@
 	require_once 'bd.php';
 	comprobar_sesion();
     
+	function cargar_categoria($codCat){
+		$res = leer_config(dirname(__FILE__)."/configuracion.xml", dirname(__FILE__)."/configuracion.xsd");
+		$bd = new PDO($res[0], $res[1], $res[2]);
+		$ins = "select nombre, descripcion from categorias where codcat = $codCat";
+		$resul = $bd->query($ins);	
+		if (!$resul) {
+			return FALSE;
+		}
+		if ($resul->rowCount() === 0) {    
+			return FALSE;
+		}	
+		//devuelve la fila de la categoría
+		return $resul->fetch();	
+	}
+	
+	function cargar_productos_categoria($codCat){
+		$res = leer_config(dirname(__FILE__)."/configuracion.xml", dirname(__FILE__)."/configuracion.xsd");
+		$bd = new PDO($res[0], $res[1], $res[2]);	
+		$sql = "select * from productos where categoria  = $codCat";	
+		$resul = $bd->query($sql);	
+		if (!$resul) {
+			return FALSE;
+		}
+		if ($resul->rowCount() === 0) {    
+			return FALSE;
+		}	
+		//si hay 1 o más
+		return $resul;			
+	}
 ?>
 <!DOCTYPE html>
 <html>
