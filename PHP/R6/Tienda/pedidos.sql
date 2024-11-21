430896deaf3a4d47f4c4e6a060042cbb1c05b514
lugosi92 <khaledbeno922@gmail.com>
2024-11-21 03:08:03 +0100

    Aplicacion PHP

--- PHP/R6/Tienda/pedidos.sql
+++ PHP/R6/Tienda/pedidos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-11-2024 a las 13:59:03
+-- Tiempo de generación: 21-11-2024 a las 03:07:10
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -24,13 +24,13 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `categoria`
+-- Estructura de tabla para la tabla `categorias`
 --
 
 CREATE TABLE `categorias` (
   `CodCat` int(11) NOT NULL,
-  `Nombre` varchar(45) NOT NULL,
-  `Descripcion` varchar(200) NOT NULL
+  `Nombre` varchar(45) DEFAULT NULL,
+  `Descripcion` varchar(200) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -41,9 +41,9 @@ CREATE TABLE `categorias` (
 
 CREATE TABLE `pedidos` (
   `CodPed` int(11) NOT NULL,
-  `Fecha` date NOT NULL,
-  `Enviado` int(11) NOT NULL,
-  `Restaurante` int(11) NOT NULL
+  `Fecha` date DEFAULT NULL,
+  `Enviada` tinyint(1) DEFAULT NULL,
+  `Restaurante` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -53,10 +53,9 @@ CREATE TABLE `pedidos` (
 --
 
 CREATE TABLE `pedidosproductos` (
-  `CodPedProd` int(11) NOT NULL,
   `Pedido` int(11) NOT NULL,
   `Producto` int(11) NOT NULL,
-  `Unidades` int(11) NOT NULL
+  `Unidades` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -67,11 +66,11 @@ CREATE TABLE `pedidosproductos` (
 
 CREATE TABLE `productos` (
   `CodProd` int(11) NOT NULL,
-  `Nombre` varchar(45) NOT NULL,
-  `Descripcion` varchar(90) NOT NULL,
-  `Peso` double NOT NULL,
-  `Stock` int(11) NOT NULL,
-  `Categoria` int(11) NOT NULL
+  `Nombre` varchar(45) DEFAULT NULL,
+  `Descripcion` varchar(200) DEFAULT NULL,
+  `Peso` double DEFAULT NULL,
+  `Stock` int(11) DEFAULT NULL,
+  `Categoria` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -82,20 +81,103 @@ CREATE TABLE `productos` (
 
 CREATE TABLE `restaurantes` (
   `CodRes` int(11) NOT NULL,
-  `Correo` varchar(90) NOT NULL,
-  `Clave` varchar(45) NOT NULL,
-  `Pais` varchar(45) NOT NULL,
-  `CP` int(11) NOT NULL,
-  `Ciudad` varchar(45) NOT NULL,
-  `Direccion` varchar(200) NOT NULL
+  `Correo` varchar(90) DEFAULT NULL,
+  `Clave` varchar(45) DEFAULT NULL,
+  `Pais` varchar(45) DEFAULT NULL,
+  `CP` int(11) DEFAULT NULL,
+  `Ciudad` varchar(45) DEFAULT NULL,
+  `Direccion` varchar(200) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `restaurantes`
+-- Índices para tablas volcadas
 --
 
-INSERT INTO `restaurantes` (`CodRes`, `Correo`, `Clave`, `Pais`, `CP`, `Ciudad`, `Direccion`) VALUES
-(1, 'khaled@gmail.com', '1234', 'España', 28942, 'Madrid', 'Gran Via');
+--
+-- Indices de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`CodCat`),
+  ADD UNIQUE KEY `Nombre` (`Nombre`);
+
+--
+-- Indices de la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD PRIMARY KEY (`CodPed`),
+  ADD KEY `FK_Restaurante` (`Restaurante`);
+
+--
+-- Indices de la tabla `pedidosproductos`
+--
+ALTER TABLE `pedidosproductos`
+  ADD PRIMARY KEY (`Pedido`,`Producto`),
+  ADD KEY `FK_Producto` (`Producto`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`CodProd`),
+  ADD KEY `FK_Categoria` (`Categoria`);
+
+--
+-- Indices de la tabla `restaurantes`
+--
+ALTER TABLE `restaurantes`
+  ADD PRIMARY KEY (`CodRes`),
+  ADD UNIQUE KEY `Correo` (`Correo`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `CodCat` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  MODIFY `CodPed` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `CodProd` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `restaurantes`
+--
+ALTER TABLE `restaurantes`
+  MODIFY `CodRes` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD CONSTRAINT `FK_Restaurante` FOREIGN KEY (`Restaurante`) REFERENCES `restaurantes` (`CodRes`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `pedidosproductos`
+--
+ALTER TABLE `pedidosproductos`
+  ADD CONSTRAINT `FK_Pedido` FOREIGN KEY (`Pedido`) REFERENCES `pedidos` (`CodPed`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_Producto` FOREIGN KEY (`Producto`) REFERENCES `productos` (`CodProd`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD CONSTRAINT `FK_Categoria` FOREIGN KEY (`Categoria`) REFERENCES `categorias` (`CodCat`) ON DELETE SET NULL;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ PHP/R6/Tienda/procesar_pedido.php
@@ -0,0 +1,29 @@
+<?php
+	/*comprueba que el usuario haya abierto sesión o redirige*/
+	require 'sesiones.php';
+	require_once 'bd.php';
+	comprobar_sesion();
+?>	
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset = "UTF-8">
+		<title>Pedidos</title>		
+	</head>
+	<body>
+	<?php 
+	require 'cabecera.php';			
+	$resul = insertar_pedido($_SESSION['carrito'], $_SESSION['usuario']['codRes']);
+	if($resul === FALSE){
+		echo "No se ha podido realizar el pedido<br>";			
+	}else{
+		$correo = $_SESSION['usuario']['correo'];
+		echo "Pedido realizado con éxito. Se enviará un correo de confirmación a: $correo ";													
+		//vaciar carrito	
+		$_SESSION['carrito'] = [];
+
+		}		
+	?>		
+	</body>
+</html>
+	
