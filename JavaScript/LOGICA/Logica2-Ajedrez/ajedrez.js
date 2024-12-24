75852e2dbfd317eb2c0ff4cabe7c6dd65bb3cb7f
lugosi92 <khaledbeno922@gmail.com>
2024-12-24 09:55:48 -0500

    JS-Ajedrez

--- JavaScript/LOGICA/Logica2-Ajedrez/ajedrez.js
+++ JavaScript/LOGICA/Logica2-Ajedrez/ajedrez.js
@@ -37,19 +37,74 @@ const juveniles = [];
 const adultos = [];
 
 
-arrayJugadores.forEach(lineas => {
-    dato = lineas.split(";");
+arrayJugadores.forEach(linea => {
+    dato = linea.split(";");
 
-    
+    if(dato[2] <= 18){
+        juveniles.push(linea);
+    }else{
+        adultos.push(linea);
+    }
 });
 
-console.table(lineas);
+console.log(juveniles);
 
 
 
 
+ /*----------------------------------PASO 3 - CREAR POSICIONES-------------------------------------------*/
+
+ const equipos = [];
+
+ const capitanJ = [];
+ const principalJ = [];
+ const suplentesJ = [];
+
+ while( juveniles.length > 7){
+    const equipo = [];
+
+    capitanJ.push(juveniles.shift());
+
+    for(let i = 0; i < 4 ; i++){
+        principalJ.push(juveniles.shift());
+    }
+
+    for(let i = 0; i < 2 ; i++){
+        suplentesJ.push(juveniles.shift());
+    }
+    equipo.push(...capitanJ,...principalJ,...suplentesJ);
+    equipos.push([...equipo]); 
+
+    console.table("----CAPITAN-----");
+    console.table(capitanJ);
+    console.table("----PRINCIAPLES-----");
+    console.table(principalJ);
+    console.table("-----SUPLENTES----");
+    console.table(suplentesJ);
+    console.table("-----EQUIPO----");
+    console.table(equipo);
+    console.table("-----EQUIPOS----");
+    console.log(equipos);
 
 
+      // Vaciar las listas temporales para el próximo equipo
+      capitanJ.length = 0;
+      principalJ.length = 0;
+      suplentesJ.length = 0;
+
+ }
+
+ /*----------------------------------PASO 4 - RESERVAS-------------------------------------------*/
+
+ const reservas = [];
+
+ while(juveniles.length > 0){
+    reservas.push(juveniles.shift());
+ }
+
+
+    console.table("-----RESERVAS----");
+    console.log(reservas);
 
 
 
