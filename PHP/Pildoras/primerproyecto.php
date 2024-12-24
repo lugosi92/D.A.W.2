1eb3d82f51ee78c2eb6784dda54c78a78bb97aa4
lugosi92 <khaledbeno922@gmail.com>
2024-12-24 15:36:02 -0500

    php

--- PHP/Pildoras/primerproyecto.php
+++ PHP/Pildoras/primerproyecto.php
@@ -6,6 +6,46 @@
     <title>Document</title>
 </head>
 <body>
-    "<?php echo "Mi primer proyecto" ?>"
+    <?php 
+    
+    echo "Mi primer proyecto <br>"; 
+
+    echo "Hola curso <br>";
+
+    echo "Hasta la prxima <br>";
+
+ /*------------------------------------VIDEO 4 VARIABLES Y COMENTARIOS---------------------------------------------------*/
+    /*
+        1. Comentarios
+        2. Variables 
+        3. Imprimir
+    */
+
+    // 1. Comentario
+ 
+    // 2. Variables (Su valor cambia durante la ejecucion dle programa)
+        // $1nombre -> No es valido , no puede empezar por numeros
+            //Comilla simple ->  interpreta literlamente
+            //Comilla doble ->  interpreta variables en un string 
+
+    $nombre = "Juan";
+    $edad = 18;
+
+    // 3. Imprimir (print o echo && concatenacion )
+
+    print "El nombre de usuario es : " .  $nombre . "<br>";
+    print "La edad de $nombre es $edad ";
+    
+
+/*------------------------------------VIDEO 5 FDLUJO DE EJECUCION---------------------------------------------------*/
+   /*
+        1. Crear servidorde pruebas (XAMP- VScode)
+        2. Introduccion funciones y flujo ejeccion 
+        3. Introduccion include y requiere
+    */
+
+    // El flujo va de arriba abajo
+    ?>
+
 </body>
 </html>
