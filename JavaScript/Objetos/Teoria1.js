dec08198920f89205ab57cce1a39f006809251d0
lugosi92 <khaledbeno922@gmail.com>
2025-01-14 00:12:15 +0100

    Ob

--- JavaScript/Objetos/Teoria1.js
+++ JavaScript/Objetos/Teoria1.js
@@ -1,101 +1,132 @@
-const cliente={
-    nombre:"Khalid",
-    apellido:"Benoukhiye",
-    telefono:"650883881",
-    ciudad:"Madrid"
+//--------------------------------------------------CREACION DE OBJETOS--------------------------------------------
+
+
+// Objeto Literal: Es la forma más sencilla de crear un objeto. Se define con llaves {} y se le asignan propiedades directamente.
+   
+const cliente = {
+    nombre: "Khalid",
+    apellido: "Benoukhiye",
+    telefono: "650883881",
+    ciudad: "Madrid"
 };
 
-const viaje={
-    origen:"Madrid",
-    destino:"Paris",
-    duracion:"7 dias",
-    pais:"Francia",
-    trayecto: function(){
-        return this.origen+"-"+this.destino;
+// Object.create(): Crea un nuevo objeto basado en otro objeto. Ideal para establecer un prototipo.
+const viajeBase = {
+    trayecto: function() {
+        return this.origen + " - " + this.destino;
+    }
+};
+console.log("-----1------");
+// Viaje hereda trayecto
+const viaje = Object.create(viajeBase);
+viaje.origen = "Madrid";
+viaje.destino = "Paris";
+viaje.duracion = "7 días";
+viaje.pais = "Francia";
+
+console.log(viaje.trayecto());  // "Madrid - Paris"
+
+// Método new Object(): Crea un objeto vacío (No se usa)
+const viaje2 = new Object();
+viaje.origen = "Madrid";
+viaje.destino = "Paris";
+viaje.duracion = "7 días";
+viaje.pais = "Francia";
+viaje.trayecto = function() {
+    return this.origen + " - " + this.destino;
+};
+
+console.log(viaje.trayecto());  // "Madrid - Paris"
+console.log("-----2------");
+
+
+//----------------------------------Definición de objetos con y sin constructores------------------------------
+
+
+//1. Objeto con Método (Sin Constructor)
+const viaje3 = {
+    origen: "Madrid",
+    destino: "Paris",
+    duracion: "7 días",
+    pais: "Francia",
+    trayecto: function() {
+        return `${this.origen} - ${this.destino}`;
     }
 };
 
-// function trayecto(viaje){
-//     console.log(viaje.origen+"-"+viaje.destino);
-// }
-// trayecto(viaje);
-
+// Imprimir propiedades y método
 for (const key in viaje) {
-   
-    if(typeof viaje[key]!="function"){
+    if (typeof viaje[key] !== "function") {
         console.log(key+ " es " + viaje[key]);
     }
-    
-}
-console.log("-----------");
-
-function Viaje(origen,destino,duracion,pais){
-    this.origen=origen;
-    this.destino=destino;
-    this.duracion=duracion;
-    this.pais=pais;
 }
 
-const viaje2= new Viaje("Madrid","Berlin","10 dias","Alemania");
+console.log(viaje.trayecto());  // "Madrid - Paris"
+console.log("-----3------");
 
-for (const key in viaje2) {
-   
-    if(typeof viaje2[key]!="function"){
-        console.log(key+ " es " + viaje2[key]);
+
+//2. Constructor para Crear Objetos (Con new)
+    //Un constructor es una función que inicializa propiedades y crea instancias de un objeto. Se usa la palabra clave new para instanciar el objeto.
+
+    // Constructor
+    function Viaje(origen, destino, duracion, pais) {
+        this.origen = origen;
+        this.destino = destino;
+        this.duracion = duracion;
+        this.pais = pais;
     }
     
-}
-
-console.log("-----------");
-
-
-// VIAJES
-
-const Mallorca = new Viaje("Madrid", "Mallorca", "10 dias", "España");
-console.log(Mallorca);
-const Londres = new Viaje("Madrid", "Londres", "10 dias", "Reino Unido");
-console.log(Londres);
-const Burgos = new Viaje("Madrid", "Burgos", "10 dias", "España");
-console.log(Burgos);
-const Toledo = new Viaje("Madrid", "Toledo", "10 dias", "España");
-console.log(Toledo);
-// Borrar
-
-delete(Toledo.pais);
-Toledo.pais="España";
+    // Instanciación de objetos
+    const berlin = new Viaje("Madrid", "Berlin", "10 días", "Alemania");
+    const mallorca = new Viaje("Madrid", "Mallorca", "10 días", "España");
+    
+    // Imprimir propiedades
+    for (const key in berlin) {
+        if (typeof berlin[key] !== "function") {
+            console.log(key+ " es " + viaje[key]);
+        }
+    }
+console.log("-----4------");
 
 
-console.log("-----------");
 
-// constructor
-function cliente2(nombre, apellido,ciudad,miViaje,telefono, contratado){
-    this.nombre=nombre;
-    this.apellido=apellido;
-    this.ciudad=ciudad;
-    this.miViaje = miViaje;
+
+//3. Uso de delete para Eliminar Propiedades
+    //Se puede eliminar propiedades de un objeto utilizando delete.
+
+const toledo = new Viaje("Madrid", "Toledo", "10 días", "España");
+console.log(toledo);  // { origen: "Madrid", destino: "Toledo", duracion: "10 días", pais: "España" }
+
+delete toledo.pais;  // Eliminar propiedad
+toledo.pais = "España";  // Añadirla de nuevo
+
+console.log(toledo);  // { origen: "Madrid", destino: "Toledo", duracion: "10 días", pais: "España" }
+
+console.log("-----5------");
+
+
+//4. Constructor con Métodos
+    //Si necesitas añadir métodos dentro del constructor, puedes hacerlo de esta forma:
+function Cliente(nombre, apellido, ciudad, telefono, viaje) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.ciudad = ciudad;
     this.telefono = telefono;
-    this.contratado = function(){
-        return miViaje ? true : false;
+    this.miViaje = viaje;
+    this.contratado = function() {
+        return this.miViaje ? true : false;
+    };
+}
+    
+const javier = new Cliente("Javier", "Pedrosa", "Madrid", "666666666", "Avila");
+    
+console.log(javier.contratado());  // true si tiene un viaje asignado
+    
+// Imprimir propiedades
+for (const key in javier) {
+    if (typeof javier[key] !== "function") {
+        console.log(key+ " es " + viaje[key]);
     }
 }
 
-const Javier = new cliente2("Javier", "Pedrosa", "Madrid",  "Avila","666666666");
-
-// Imprimir
-for (const key in Javier) {
-
-    if(typeof Javier[key] != "function"){
-        console.log(key + " es " + Javier[key]);    
-    }
-}
-console.log(Javier.contratado());
-
-
-const Rodri = new cliente2("Javier", "Pedrosa", "Madrid",  Burgos,"666666666");
-
-console.log(Burgos.duracion);
-
-
-
-
-
+console.log("-----6------");
