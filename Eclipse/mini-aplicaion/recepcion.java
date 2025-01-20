daace394fcbfe8beb4ec52bcb2edee12b6a0cdeb
lugosi92 <khaledbeno922@gmail.com>
2025-01-20 12:38:43 +0100

    Modificacion de recepcion

--- Eclipse/mini-aplicaion/recepcion.java
+++ Eclipse/mini-aplicaion/recepcion.java
@@ -28,22 +28,68 @@ public class recepcion extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
+		/*
+		 Para la contraseña:
+    		• Debe introducir al menos 12 caracteres.
+    		• Sólo puede introducir 20 caracteres.
+    		• Debe introducir al menos 1 letra mayúscula.
+    		• Debe introducir al menos 1 letra minúscula.
+    		• Debe introducir al menos 1 carácter especial.
+    		• Se permiten los siguientes caracteres especiales -_/()$%&?!+*#<>=
+    		• Debe introducir al menos 1 carácter numérico
+		 */
 		String nombre = request.getParameter("usuario");
 		String clave = request.getParameter("clave");
 		
 		String error = " ";
 		String errorGeneral = "false";
 		
-		if(nombre == null || nombre.isEmpty()) {
-			error += "Introduzca un nombre valido<br>";
+		//NOMBRES
+		if(nombre.length() < 6 || nombre.length() > 30) {
+			error += "Usuario incorrecto (1)<br>";
+			errorGeneral = "true";
+		}
+		if(nombre.isEmpty()) {
+			error += "Usuario incorrecto (2)<br>";
+			errorGeneral = "true";
+		}
+		if(nombre.contains(" ")) {
+			error += "Usuario incorrecto (3)<br>";
 			errorGeneral = "true";
 		}
 		
-		if(clave == null || clave.isEmpty()) {
-			error += "Introduzca una clave valido";
+		if(!nombre.matches("[A-Za-z0-9-_]+")) {
+			error += "Usuario incorrecto (4)<br>";
 			errorGeneral = "true";
 		}
 		
+		//CONTRASEÑAS
+		if(clave.length() < 12 || clave.length() > 20) {
+			error += "Introduzca una clave valida(1)<br>";
+			errorGeneral = "true";
+		}
+		
+		if (!clave.matches(".*[A-Z].*")) {
+		    error += "Introduzca una clave valida(2)<br>";
+		    errorGeneral = "true";
+		}
+
+		if (!clave.matches(".*[a-z].*")) {
+		    error += "Introduzca una clave valida(3)<br>";
+		    errorGeneral = "true";
+		}
+
+		if (!clave.matches(".*[0-9].*")) {
+		    error += "Introduzca una clave valida(4)<br>";
+		    errorGeneral = "true";
+		}
+
+		if (!clave.matches(".*[-_/()$%&?!+*#<>=].*")) {
+		    error += "Introduzca una clave valida(5)<br>";
+		    errorGeneral = "true";
+		}
+
+
 
 		
 		request.getSession().setAttribute("error", error);
