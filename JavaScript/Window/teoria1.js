42afe33f2528c85bb99ac5ee74347c739002ff5c
lugosi92 <khaledbeno922@gmail.com>
2025-01-27 09:58:21 +0100

    K

---
+++ JavaScript/Window/teoria1.js
@@ -0,0 +1,53 @@
+//Esquema de BOM-DOM
+
+/*
+windows 
+screen  histori, location, navigator, console 
+*/
+//WINDOW
+//Inner: el tamaño del navegador usandose
+window.innerHeight;
+window.innerWidth;
+
+//Outer: El tamaño de la ventana , (condols + utl...)
+
+
+scroll(0,500)
+
+getSelection().toString()
+
+find("Alphafly")
+true
+
+open("https://adidas.es")
+
+scrol(0,200)
+scrollBy(0,200)
+
+//NAVIGATOR
+navigator.userAgent
+'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36'
+
+navigator.cookieEnabled
+true
+
+navigator.storage
+
+//screen
+
+screen.height
+screen.width
+
+screen.color
+
+//location
+
+location.href
+location.hostname
+//recagrfagr la pagina
+
+
+//history
+
+history.go(-1)
+history.length
